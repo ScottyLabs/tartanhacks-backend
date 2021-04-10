96c5a6132ee0a80ed72b55b3f49183d647428273
Gram Liu <gram.liu226@gmail.com>
2021-04-10 15:17:35 -0400

    Initialized typescript+express

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+const PORT = 5000;
+
+const app = express();
+
+app.get("/", (_, res) => {
+  res.status(200).send();
+})
+
+app.listen(PORT, () => console.log(`Running on port ${PORT}`))
