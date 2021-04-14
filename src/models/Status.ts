55e1f2e7bd24e1d4fd44892666495aef346aa78c
Gram Liu <gram.liu226@gmail.com>
2021-04-14 12:55:34 -0400

    Fixed typo

---
+++ src/models/Status.ts
@@ -0,0 +1,33 @@
+import { model, Schema } from "mongoose"
+
+/**
+ * Status of a user in the registration process
+ */
+const Status: Schema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  event: {
+    type: Schema.Types.ObjectId,
+    ref: "Event",
+    required: true
+  },
+  verified: Boolean,
+  completedProfile: Boolean,
+  admitted: Boolean,
+  admittedBy: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  },
+  confirmed: Boolean,
+  declined: Boolean
+}, {
+  timestamps: {
+    createdAt: 'createdAt',
+    updatedAt: 'updatedAt'
+  }
+})
+
+export default model("Status", Status)
