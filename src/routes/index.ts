7f6fb2ea2fa59550b069d741debefffdff3491b3
anudaweerasinghe <anuda@live.com>
2021-06-29 17:21:25 +0530

    implemented schedule/new endpoint. Code not compiling

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 import authRouter from "./auth";
 import adminRouter from "./admin";
 import usersRouter from "./users";
+import scheduleRouter from "./schedule";
 
 const router = express.Router();
 router.use("/auth", authRouter);
 router.use("/admin", adminRouter);
 router.use("/users", usersRouter);
+router.use("/schedule", scheduleRouter);
 
 export default router;
