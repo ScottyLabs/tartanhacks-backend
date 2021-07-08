1bee97af7b184f421d9f9a4719db817575245f60
anudaweerasinghe <anuda@live.com>
2021-07-08 23:00:46 +0530

    setup teams route and controller

---
+++ src/controllers/TeamsController.ts
@@ -0,0 +1,4 @@
+import Team from "src/models/Team";
+import { bad, error } from "../util/error";
+import { Request, Response } from "express";
+import { getTartanHacks } from "./EventController";
---
+++ src/routes/teams.ts
@@ -0,0 +1,14 @@
+import express, { Router } from "express";
+import {} from "../controllers/TeamsController";
+import {} from "./middleware";
+
+const router: Router = express.Router();
+
+/**
+ * @swagger
+ * tags:
+ *  name: Teams Module
+ *  description: Endpoints to manage team information.
+ */
+
+export default router;
