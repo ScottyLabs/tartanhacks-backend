9c96edc396090fe9f2580c1537ed9468054a6e9a
Gram Liu <gram.liu226@gmail.com>
2021-10-31 03:19:19 -0400

    Added admission tests

--- tests/routes/users/admission.test.ts
+++ tests/routes/users/admission.test.ts
@@ -142,3 +142,109 @@ describe("admission", () => {
     expect(status.admitted).toBeUndefined();
   });
 });
+
+describe("rejection", () => {
+  it("should work for admins", async () => {
+    // Register user to reject
+    const registerResponse = await request(app).post("/auth/register").send({
+      email: `dummy5@scottylabs.org`,
+      password: "abc123",
+    });
+    expect(registerResponse.status).toEqual(200);
+    const { _id: userId } = registerResponse.body;
+
+    // Update status so user appears to have completed profile
+    await StatusController.updateStatus(
+      new ObjectId(userId),
+      StatusField.VERIFIED,
+      true
+    );
+    await StatusController.updateStatus(
+      new ObjectId(userId),
+      StatusField.COMPLETED_PROFILE,
+      true
+    );
+
+    // Reject user
+    const admitResponse = await request(app)
+      .post(`/users/${userId}/reject`)
+      .set("x-access-token", adminToken);
+    expect(admitResponse.status).toEqual(200);
+
+    // Check that status is false
+    const status = await Status.findOne({ user: new ObjectId(userId) });
+    expect(status).not.toBeNull();
+    expect(status.admitted).toStrictEqual(false);
+  });
+
+  it("should fail for incomplete profiles", async () => {
+    // Register user to reject
+    const registerResponse = await request(app).post("/auth/register").send({
+      email: `dummy6@scottylabs.org`,
+      password: "abc123",
+    });
+    expect(registerResponse.status).toEqual(200);
+    const { _id: userId } = registerResponse.body;
+
+    // Update status so user appears to have verified account
+    await StatusController.updateStatus(
+      new ObjectId(userId),
+      StatusField.VERIFIED,
+      true
+    );
+
+    // Reject user
+    const admitResponse = await request(app)
+      .post(`/users/${userId}/reject`)
+      .set("x-access-token", adminToken);
+    expect(admitResponse.status).toEqual(400);
+
+    // Check that status is not updated
+    const status = await Status.findOne({ user: new ObjectId(userId) });
+    expect(status.admitted).toBeUndefined();
+  });
+
+  it("should be inaccessible to non-admins", async () => {
+    // Register user to attempt rejection
+    const registerAttackerResponse = await request(app)
+      .post("/auth/register")
+      .send({
+        email: `dummy7@scottylabs.org`,
+        password: "abc123",
+      });
+    expect(registerAttackerResponse.status).toEqual(200);
+    const { token: attackerToken } = registerAttackerResponse.body;
+
+    // Register user to reject
+    const registerApplicantResponse = await request(app)
+      .post("/auth/register")
+      .send({
+        email: `dummy8@scottylabs.org`,
+        password: "abc123",
+      });
+    expect(registerApplicantResponse.status).toEqual(200);
+    const { _id: userId } = registerApplicantResponse.body;
+
+    // Update status so user appears to have verified account
+    await StatusController.updateStatus(
+      new ObjectId(userId),
+      StatusField.VERIFIED,
+      true
+    );
+    await StatusController.updateStatus(
+      new ObjectId(userId),
+      StatusField.COMPLETED_PROFILE,
+      true
+    );
+
+    // Attempt rejecting user
+    const admitResponse = await request(app)
+      .post(`/users/${userId}/reject`)
+      .set("x-access-token", attackerToken);
+    expect(admitResponse.status).toEqual(403);
+
+    // Check that status is not updated
+    const status = await Status.findOne({ user: new ObjectId(userId) });
+    expect(status.admitted).toBeUndefined();
+  });
+});
