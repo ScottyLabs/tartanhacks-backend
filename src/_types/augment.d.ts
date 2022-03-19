3f3e10c05ad57f0cf76a68da1380d144cddb5ad9
Gram Liu <gram.liu226@gmail.com>
2022-03-19 15:40:32 -0400

    Updated env template and augment type

--- src/_types/augment.d.ts
+++ src/_types/augment.d.ts
@@ -13,6 +13,7 @@ declare global {
       PORT: string;
       BACKEND_URL: string;
       FRONTEND_URL: string;
+      DISCORD_URL: string;
       JWT_SECRET: string;
       AUTH_TOKEN_EXPIRY: string;
       MONGODB_URI: string;
@@ -24,13 +25,10 @@ declare global {
       EMAIL_TLS?: string;
       EMAIL_CONTACT?: string;
       EMAIL_HEADER_IMAGE?: string;
-      // Google drive credentials
-      DRIVE_FOLDER_ID?: string;
-      DRIVE_CLIENT_ID?: string;
-      DRIVE_CLIENT_SECRET?: string;
-      DRIVE_ACCESS_TOKEN?: string;
-      DRIVE_REFRESH_TOKEN?: string;
-      DRIVE_TOKEN_EXPIRY_DATE?: string;
+      // Google Cloud credentials
+      GCP_CLIENT_EMAIL?: string;
+      GCP_PRIVATE_KEY?: string;
+      GCP_PROJECT_ID?: string;
     }
   }
 }
