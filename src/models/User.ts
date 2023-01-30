fe8f7e76ada0a927c18fa288a845739697b8d826
pm3512 <aobolens@andrew.cmu.edu>
2023-01-29 23:44:34 -0500

    added todo

--- src/models/User.ts
+++ src/models/User.ts
@@ -130,6 +130,7 @@ User.methods.setStatus = async function (status: Status): Promise<void> {
   const statusLevel = getStatusLevel(status);
   const userStatusLevel = getStatusLevel(this.status);
 
+  // TODO change to >=
   if (userStatusLevel > statusLevel) {
     throw new Error(
       `Cannot revert status! Attempting to go backwards from ${this.status} to ${status}`
