e63a9be9e1fab98d027bd2bd6c7de4112702b346
Gram Liu <gram.liu226@gmail.com>
2023-02-09 03:00:31 -0500

    Create database seed script

---
+++ prisma/seed.ts
@@ -0,0 +1,46 @@
+import { PrismaClient, Settings } from "@prisma/client";
+import { parameters } from "../src/settings.json";
+
+const prisma = new PrismaClient();
+
+type SettingsCreateInput = Omit<Settings, "id">;
+type SettingsConfig = typeof parameters;
+type SettingsKey = keyof SettingsConfig;
+
+/**
+ * Create the initial settings
+ */
+async function createSettings() {
+  const settingsKeys = Object.keys(parameters) as SettingsKey[];
+  const settingsInput = settingsKeys.reduce(
+    (setting, key) => ({
+      ...setting,
+      [key]: parameters[key]["value"],
+    }),
+    {} as SettingsCreateInput
+  );
+
+  await prisma.settings.upsert({
+    where: {},
+    update: {},
+    create: settingsInput,
+  });
+}
+
+/**
+ * Seed the database with initial entities
+ */
+async function main() {
+  // Create settings
+  await createSettings();
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
