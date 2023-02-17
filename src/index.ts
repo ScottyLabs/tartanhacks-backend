eafbfae1fa897a09f0eb69a4a72d11fb2fb1f9aa
Gram Liu <gram.liu226@gmail.com>
2023-02-17 11:55:16 -0500

    Create downloadResumes function

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
+import axios, { AxiosResponse } from "axios";
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import { writeFileSync } from "fs";
 import mongoose from "mongoose";
+import { ObjectId } from "bson";
 import morgan from "morgan";
 import swaggerUi from "swagger-ui-express";
+import User from "./models/User";
 import router from "./routes";
+import { getResumeUrl } from "./services/storage";
 import swaggerSpecification from "./swagger";
 import { startup } from "./util/startup";
-import cors from "cors";
-import Team from "./models/Team";
-import Profile from "./models/Profile";
+import { Status } from "./_enums/Status";
 
 dotenv.config();
 
@@ -60,5 +64,36 @@ const server = app.listen(PORT, async () => {
     return;
   }
 
+  // await downloadResumes();
+
   console.log(`Running on port ${PORT}`);
 });
+
+async function downloadResumes() {
+  const users = await User.find({
+    // status: Status.CONFIRMED,
+    admin: false,
+    company: null,
+  });
+  const ids = users.map((user) => user._id);
+  const urls = [] as [ObjectId, string][];
+  for (const [idx, id] of ids.entries()) {
+    console.log(`Getting url ${idx + 1}/${ids.length}`);
+    urls.push([id, await getResumeUrl(id)]);
+  }
+
+  const entries = [] as [ObjectId, AxiosResponse<any, any>][];
+  for (const [idx, [id, url]] of urls.entries()) {
+    try {
+      console.log(`Downloading resume ${idx + 1}/${urls.length}`);
+      entries.push([id, await axios.get(url, { responseType: "arraybuffer" })]);
+    } catch (err) {
+      console.log("Failure");
+    }
+  }
+
+  entries.forEach(([id, response], idx) => {
+    console.log("Writing resume", idx + 1, "/", entries.length);
+    writeFileSync(`resumes/${id}.pdf`, response.data);
+  });
+}
